e385c07582b023cb5e9d5876ca0cdf2b7f8c61b9
Your Name <you@example.com>
2026-02-10 17:13:12 +0530

    Project update

--- src/resources/projects.js
+++ src/resources/projects.js
@@ -10,35 +10,35 @@ const projectsData = [
       title: "serenity yoga landing page",
       image: "/yoga.png",
       
-      link: "https://github.com/SHADOW712345678910/project-2",
+      link: "https://github.com/shardool598-jpg/yoGo-land",
     }
     ,
     {
       title: "Spotify Clone",
       image: "/clone.png",
 
-      link: "https://github.com/SHADOW712345678910/project_1",
+      link: "https://github.com/shardool598-jpg/spotify",
     }
     ,
     {
       title: "food delivery website",
       image: "/fooddelivery.png",
       
-      link: "https://github.com/SHADOW712345678910/project_3",
+      link: "https://github.com/shardool598-jpg/food-app",
     }
     ,
     {
       title: "Ecommerce website",
       image: "/Screenshot 2025-04-17 161508.png",
       
-      link: "https://github.com/SHADOW712345678910/ecommerce-website",
+      link: "https://github.com/shardool598-jpg/e-commerce-website",
     }
     ,
     {
       title: "TO-do list website",
       image: "/todo.png",
       
-      link: "https://github.com/SHADOW712345678910/TO-do_List",
+      link: "https://github.com/shardool598-jpg/to-do-list",
     }
 
 
